1ffb0ff69fd4bae1bdb9fd8cf05100c10ff08060
Adam Szerszen <szerszen_67115@cloud.wsb.wroclaw.pl>
2020-03-28 01:00:56 +0100

    Fixed store.js

--- store.js
+++ store.js
@@ -1,13 +1,13 @@
 const storage = require('azure-storage')
-const service = storage.createTableService()
-const table = 'tasks'
 var retryOperations = new azure.ExponentialRetryPolicyFilter(1);
+const service = storage.createTableService().withFilter(retryOperations)
+const table = 'tasks'
 
 const init = async () => (
     new Promise((resolve, reject) => {
         service.createTableIfNotExists(table, (error, result, response) => {
             !error ? resolve() : reject()
-        }).withFilter(retryOperations);
+        })
     })
 )
 
