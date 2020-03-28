626c70aeba9792f2c4280720db54709d84528e15
Adam Szerszen <szerszen_67115@cloud.wsb.wroclaw.pl>
2020-03-28 01:27:08 +0100

    added filter to log table manipulations

--- store.js
+++ store.js
@@ -3,6 +3,15 @@ var retryOperations = new storage.ExponentialRetryPolicyFilter(1);
 const service = storage.createTableService().withFilter(retryOperations)
 const table = 'tasks'
 
+function LoggingFilter() {
+    this.handle = (requestOptions, next) => {
+        console.log(requestOptions)
+        next(requestOptions, (returnObject, finalCallback, next) => {
+            console.log(returnObject)
+        })
+    }
+}
+
 const init = async () => (
     new Promise((resolve, reject) => {
         service.createTableIfNotExists(table, (error, result, response) => {
